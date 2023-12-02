61e0a32b129a1e69ae232f6793ae3604fb39e5b3
bh-santos <brendondias.office@gmail.com>
2023-12-01 22:24:58 -0300

    feat: added more config

--- core/src/com/nnw/game/util/Settings.java
+++ core/src/com/nnw/game/util/Settings.java
@@ -1,24 +1,28 @@
 package com.nnw.game.util;
 
 public class Settings {
-    public static final Double GAME_WIDTH = 1280.0;
-    public static final Double GAME_HEIGHT = 720.0;
+    public static final int GAME_WIDTH = 1280;
+    public static final int GAME_HEIGHT = 720;
 
-    public static final Double PLAYER_WIDTH = 0.0;
-    public static final Double PLAYER_HEIGHT = 0.0;
+    public static final int FOREGROUND_FPS = 60;
 
-    public static final Double SUBMENU_WIDTH = 900.0;
-    public static final Double SUBMENU_HEIGHT = 600.0;
+    public static final int IDLE_FPS = 60;
 
-    public static final Double MENU_BAR_WIDTH = 900.0;
-    public static final Double MENU_BAR_HEIGHT = 30.0;
+    public static final int PLAYER_WIDTH = 0;
+    public static final int PLAYER_HEIGHT = 0;
 
-    public static final Double CONTENT_SUBMENU_WIDTH = 900.0;
-    public static final Double CONTENT_SUBMENU_HEIGHT = 570.0;
+    public static final int SUBMENU_WIDTH = 900;
+    public static final int SUBMENU_HEIGHT = 600;
 
-    public static final Double MENU_PANELS_WIDTH = 350.0;
-    public static final Double MENU_PANELS_HEIGHT = 360.0;
+    public static final int MENU_BAR_WIDTH = 900;
+    public static final int MENU_BAR_HEIGHT = 30;
 
-    public static final Double MENU_PANELS_HEIGHT_EXTENDED=460.0;
+    public static final int CONTENT_SUBMENU_WIDTH = 900;
+    public static final int CONTENT_SUBMENU_HEIGHT = 570;
+
+    public static final int MENU_PANELS_WIDTH = 350;
+    public static final int MENU_PANELS_HEIGHT = 360;
+
+    public static final int MENU_PANELS_HEIGHT_EXTENDED=460;
 }
 
--- desktop/src/com/nnw/game/DesktopLauncher.java
+++ desktop/src/com/nnw/game/DesktopLauncher.java
@@ -3,6 +3,9 @@ package com.nnw.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.nnw.game.NNWGame;
+import com.nnw.game.util.Settings;
+
+import static com.nnw.game.util.Settings.*;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +13,9 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Naruto Ninja War");
+		config.setResizable(false);
+		config.setWindowedMode(GAME_WIDTH, GAME_HEIGHT);
+
 		new Lwjgl3Application(new NNWGame(), config);
 	}
 }
