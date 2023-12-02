f3bba1dd3dd74c299676e496b3a7fbcae9269d0f
bh-santos <brendondias.office@gmail.com>
2023-12-01 22:58:59 -0300

    feat: more config

--- core/src/com/nnw/game/NNWGame.java
+++ core/src/com/nnw/game/NNWGame.java
@@ -1,11 +1,17 @@
 package com.nnw.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.nnw.game.ui.MainMenu;
+import com.nnw.game.util.AssetNames;
 
 public class NNWGame extends Game {
 
@@ -13,6 +19,9 @@ public class NNWGame extends Game {
 	public void create () {
 		MainMenu mainMenu = new MainMenu(this);
 		setScreen(mainMenu);
+
+
 	}
+
 
 }
--- desktop/src/com/nnw/game/DesktopLauncher.java
+++ desktop/src/com/nnw/game/DesktopLauncher.java
@@ -2,20 +2,26 @@ package com.nnw.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nnw.game.NNWGame;
-import com.nnw.game.util.Settings;
 
+import static com.nnw.game.util.AssetNames.Textures.ANIMATED_SCREEN_ICON;
 import static com.nnw.game.util.Settings.*;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setForegroundFPS(FOREGROUND_FPS);
+		config.setIdleFPS(IDLE_FPS);
+		config.setDecorated(true);
+		config.setWindowIcon(ANIMATED_SCREEN_ICON[5]);
 		config.setTitle("Naruto Ninja War");
 		config.setResizable(false);
 		config.setWindowedMode(GAME_WIDTH, GAME_HEIGHT);
 
 		new Lwjgl3Application(new NNWGame(), config);
+
+
 	}
+
+
 }
