4a3b66faadc4d69ee2b9e54e3c82c7e08fdf8509
bh-santos <brendondias.office@gmail.com>
2023-12-01 22:15:25 -0300

    feat: added base config file

--- core/src/com/nnw/game/util/Settings.java
+++ core/src/com/nnw/game/util/Settings.java
@@ -1,4 +1,24 @@
 package com.nnw.game.util;
 
 public class Settings {
+    public static final Double GAME_WIDTH = 1280.0;
+    public static final Double GAME_HEIGHT = 720.0;
+
+    public static final Double PLAYER_WIDTH = 0.0;
+    public static final Double PLAYER_HEIGHT = 0.0;
+
+    public static final Double SUBMENU_WIDTH = 900.0;
+    public static final Double SUBMENU_HEIGHT = 600.0;
+
+    public static final Double MENU_BAR_WIDTH = 900.0;
+    public static final Double MENU_BAR_HEIGHT = 30.0;
+
+    public static final Double CONTENT_SUBMENU_WIDTH = 900.0;
+    public static final Double CONTENT_SUBMENU_HEIGHT = 570.0;
+
+    public static final Double MENU_PANELS_WIDTH = 350.0;
+    public static final Double MENU_PANELS_HEIGHT = 360.0;
+
+    public static final Double MENU_PANELS_HEIGHT_EXTENDED=460.0;
 }
+
